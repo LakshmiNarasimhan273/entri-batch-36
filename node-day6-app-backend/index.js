67036c2f0f43f847a70e41f23fd426d1e04da229
LakshmiNarasimhan273 <bachelorofphotography@gmail.com>
2025-10-08 19:38:10 +0530

    Node MongoDB Basic CRUD API's added

--- node-day6-app-backend/index.js
+++ node-day6-app-backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 require("dotenv").config();
 const dbConnection = require("./config/dbConnection");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
+// use middleware
+app.use(express.json());
+app.use("/api", userRoutes);
+
 dbConnection();
 app.listen(process.env.port, () => {
-    console.log(`Server running on ${process.env.port}`);
+    console.log(`Server running on http://localhost:${process.env.port}`);
 })
--- node-day6-app-backend/model/userModel.js
+++ node-day6-app-backend/model/userModel.js
@@ -14,8 +14,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    mobilenumner: {
+    mobilenumber: {
         type: Number,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
         required: true
     }
 });
